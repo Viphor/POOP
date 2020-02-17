ce4bfb7ce53a362692ebc0cfa8acf335bb55ea62
Andreas H. Klostergaard <akl@klostergaard.net>
2020-02-17 08:53:11 +0100

    tmp commit

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -6,6 +6,20 @@ mod error;
 
 type Output<Out = ()> = Result<Out, error::CodegenError>;
 
+pub struct Codegen {
+    context: LLVMContextRef,
+    module: LLVMModuleRef,
+    builder: LLVMBuilderRef,
+}
+
+impl Codegen {
+    fn new<T: Into<[u8]>>(module_name: T) -> Self {
+        
+    }
+
+    fn new_with_context(context: LLVMContextRef) 
+}
+
 pub fn build_expression(context: LLVMContextRef, expr: Expression) -> Output {
     unimplemented!()
 }
